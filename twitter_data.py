72d05622cebbf26e95839eb35b27db56746fdb63
saimadhu <sai@team.quadloops.com>
2014-12-22 16:17:06 +0530

    module for getting twitter data for partiuclar user

--- twitter_data.py
+++ twitter_data.py
@@ -1,23 +1,14 @@
 import tweepy
 consumer_key = 'YUV7jXYy6CfokA2Q2rEQShy3A'
 consumer_secret = 'sWZIH0gY8kmY4Dxq2fZykyP0vL99cMlogkoAAg8SKpzYtSWWyR'
-access_token = '2862312312-Mevmn5Y3FlKptTc2QgqAxFS5Z9LUp0fNKUGqGHe'
-access_token_secret = 'rJEVQCvPfJjmKRkJ1nPiDBVy6F8HU5kDQJxdko6TB8weS'
+access_token = '2862312312-2JWFBNB3ghJBaZGpZSBlfUWYKOsyDQhxKifajzG'
+access_token_secret = '0hFQtyOMgyslDdywGaY9rU71a40n394BrqDUzN07UOM6o'
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 # Creation of the actual interface, using authentication
 #api = tweepy.API(auth,timeout=30.0)
 api = tweepy.API(auth)
-# Sample method, used to update a status
-# public_tweets = api.home_timeline()
-# print "started printing public_tweets"
-# for tweet in public_tweets:
-# 	print unicode(tweet.text).encode('ascii','ignore')
-#     #print tweet.text
-# #api.update_status('Welcome to DataAspirant!')
-print "Status Successfuly update"
-
 user = api.get_user('saimadhup')
 time_line = user.timeline()
 for single_tweet in time_line:
